feb0e23f4bd73ef58e56349446580fd553116c41
Pete Looney <petelooney81@gmail.com>
2021-01-06 12:13:46 -0600

    More Tests

--- spec.js
+++ spec.js
@@ -4,7 +4,7 @@
     //? Global created by Protractor -> used for browser-level commands like navigation w/ browser.get
     //? Browser.get is a method to launch the url we feed it.
 
-const { element } = require("protractor");
+const { element, by, browser } = require("protractor");
 
 describe('Enter name feature', () => {
     it('should enter name as Tom', () => {
@@ -13,5 +13,12 @@ describe('Enter name feature', () => {
         element(by.model('yourName')).sendKeys('Tom');//finds the element with the model name of 'yourName' and types in 'Tom'
         let helloText = element(by.xpath('/html/body/div[2]/div[1]/div[2]/div[2]/div/h1')); //finds the element with the following xpath --> inspect, right click on element you want to copy, then go to copy and then copy x-path
         expect(helloText.getText()).toEqual('Hello Tom!'); //is saying-> we are expecting this element's text to equal 'Hello Tom!' now
+    });
+});
+
+describe('Free crm title test', () => {
+    it('get title test', () => {
+        browser.get('http://www.freecrm.com');
+        expect(browser.getTitle()).toEqual('#1 Free CRM customer relationship management software cloud');
     });
 });
